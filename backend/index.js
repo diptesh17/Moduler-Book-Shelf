148082467851a158c5c07287aeeddbb5ced63590
diptesh17 <deorediptesh1883@gmail.com>
2024-01-31 09:39:06 +0530

    Inital DB setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
@@ -8,3 +8,12 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server started on Port : ${PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch(() => {
+    console.log("DB connection issue");
+  });
